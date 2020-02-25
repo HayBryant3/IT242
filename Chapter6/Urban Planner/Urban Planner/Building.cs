142f984e9fa73ca3b23517a5f70b3a9e77bf9e23
HayBryant3 <59876349+HayBryant3@users.noreply.github.com>
2020-02-25 10:13:20 -0500

    Urban Planner
    Chapter 6

---
+++ Chapter6/Urban Planner/Urban Planner/Building.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Urban_Planner
+{
+    class Building
+    {
+        private string DesignerName { get; set; }
+
+        private DateTime DateConstructed { get; set; }
+
+        private string BuildingAddress { get; set; }
+
+        private string BuildingOwner { get; set; }
+
+        public int NumberOfStories { get; set; }
+
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * NumberOfStories);
+            }
+        }
+
+        public Building (string address, string designer)
+        {
+            BuildingAddress = address;
+            DesignerName = designer;
+        }
+        
+            public void Construct()
+            {
+            DateConstructed = DateTime.Now;
+            }
+        
+        public void Purchase(string buyer)
+        {
+            BuildingOwner = buyer;
+        }
+        public void DisplayBuildingInfo()
+        {
+            Console.WriteLine(BuildingAddress);
+            Console.WriteLine("--------");
+            Console.WriteLine("Designed by" + DesignerName);
+            Console.WriteLine("Date Constructed: " + DateConstructed);
+            Console.WriteLine("Owned by: " + BuildingOwner);
+            Console.WriteLine(Volume + "cubic metres of space");
+        }
+        }
+
+    }
---
+++ Chapter6/Urban Planner/Urban Planner/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Urban_Planner
+{
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is where you make objects based on the class blueprint
+            Building MCTC = new Building("1 Mountwest Way", "Hailee Bryant")
+            {
+                Width = 100,
+                Depth = 200,
+                NumberOfStories = 4
+            };
+
+            Building CapitalBuilding = new Building("412 8th Street", "Hailee Bryant")
+            {
+                Width = 1000,
+                Depth = 250,
+                NumberOfStories = 6
+            };
+
+            Building MyHouse = new Building("1 Holler Way", "Hailee Bryant")
+            {
+                Width = 10,
+                Depth = 50,
+                NumberOfStories = 1
+            };
+
+            MCTC.Construct();
+           CapitalBuilding.Construct();
+            MyHouse.Construct();
+            MCTC.Purchase("Hailee Bryant");
+            MCTC.Purchase("Hailee Bryant");
+            MCTC.Purchase("Hailee Bryant");
+
+            MCTC.DisplayBuildingInfo();
+            CapitalBuilding.DisplayBuildingInfo();
+            MyHouse.DisplayBuildingInfo();
+
+            Console.WriteLine("this is how to use the debugger");
+        }
+    }
+}
